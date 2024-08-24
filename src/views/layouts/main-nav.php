fe7fbd804bce6c44db642e7d003546ff72fe3ce4
Vladislav <wladoxa@gmail.com>
2024-08-24 15:16:48 +0300

    Навбар

--- src/views/layouts/main-nav.php
+++ src/views/layouts/main-nav.php
@@ -12,16 +12,6 @@ $items = [
         'label' => HomeController::$title,
     ],
     [
-        'url' => Url::toRoute('statistic/index'),
-        'active' => Url::currentRoute() === 'statistic/index',
-        'label' => StatisticController::$title,
-    ],
-    [
-        'url' => Url::toRoute('statistic/detailClicks'),
-        'active' => Url::currentRoute() === 'statistic/detailClicks',
-        'label' => 'Клики',
-    ],
-    [
         'url' => Url::toRoute('user/index'),
         'active' => Url::currentRoute() === 'user/index',
         'label' => UserController::$title,
