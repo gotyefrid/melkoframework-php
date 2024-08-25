891e6e6333d294411a23deeb9b43b47afaff705c
Vladislav <wladoxa@gmail.com>
2024-08-25 21:49:57 +0300

    Новый билдер

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -15,6 +15,11 @@ class ComposerStaticInite440ee395cff48966964af05ff54db4a
         array (
             'core\\' => 5,
         ),
+        'N' => 
+        array (
+            'NilPortugues\\Sql\\QueryFormatter\\' => 32,
+            'NilPortugues\\Sql\\QueryBuilder\\' => 30,
+        ),
     );
 
     public static $prefixDirsPsr4 = array (
@@ -25,6 +30,14 @@ class ComposerStaticInite440ee395cff48966964af05ff54db4a
         'core\\' => 
         array (
             0 => __DIR__ . '/../..' . '/core',
+        ),
+        'NilPortugues\\Sql\\QueryFormatter\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/nilportugues/sql-query-formatter/src',
+        ),
+        'NilPortugues\\Sql\\QueryBuilder\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/nilportugues/sql-query-builder/src',
         ),
     );
 
