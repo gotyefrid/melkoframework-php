569b12e27e8158290f38b1a5c3ad60a82e403355
Vladislav <wladoxa@gmail.com>
2024-08-26 21:48:42 +0300

    Фикс ассетов

--- src/views/layouts/main.php
+++ src/views/layouts/main.php
@@ -12,10 +12,10 @@ use core\FlashMessageWidget;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="/src/views/layouts/css/bootstrap.css" rel="stylesheet">
-    <link rel="stylesheet" href="/src/views/layouts/css/bootstrap-icons.css">
+    <link href="src/views/layouts/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="src/views/layouts/css/bootstrap-icons.css">
     <title><?= $title ?? 'Заголовок' ?></title>
-    <script src="/src/views/layouts/js/jquery-3.7.1.js"></script>
+    <script src="src/views/layouts/js/jquery-3.7.1.js"></script>
 </head>
 <body>
 <?php require('main-nav.php') ?>
@@ -23,6 +23,6 @@ use core\FlashMessageWidget;
     <?= FlashMessageWidget::showFlashIfExist() ?>
     <?= $content ?>
 </div>
-<script src="/src/views/layouts/js/bootstrap.bundle.js"></script>
+<script src="src/views/layouts/js/bootstrap.bundle.js"></script>
 </body>
 </html>
