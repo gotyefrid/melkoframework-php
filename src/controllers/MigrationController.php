077cd600f52beb934225a25f0b3a620c05919758
Vladislav <wladoxa@gmail.com>
2024-08-26 11:36:38 +0300

    Миграции

---
+++ src/controllers/MigrationController.php
@@ -0,0 +1,45 @@
+<?php /** @noinspection PhpUnused */
+
+namespace src\controllers;
+
+use core\Application;
+use core\Controller;
+use src\models\User;
+
+class MigrationController extends Controller
+{
+    public function actionMigrate(): void
+    {
+        $this->initUserTable();
+    }
+
+    public function actionCreateUser(): void
+    {
+        $this->createUser();
+    }
+
+    private function createUser(): void
+    {
+        $user = new User();
+        $user->username = 'user' . rand(1, 999999);
+        $user->password = password_hash('admin', PASSWORD_DEFAULT);
+        $user->save();
+        echo "Юзер создан: Логин $user->username пароль admin <br>";
+    }
+
+    private function initUserTable(): void
+    {
+        $result = Application::$app->db->query("SELECT name FROM sqlite_master WHERE type='table' AND name='users';");
+
+        if ($result->fetch()) {
+            echo "Таблица users уже существует <br>";
+        } else {
+            Application::$app->db->exec("CREATE TABLE IF NOT EXISTS users (
+                    id INTEGER PRIMARY KEY,
+                    username TEXT,
+                    password TEXT
+                )");
+            echo "Таблица users создана <br>";
+        }
+    }
+}
